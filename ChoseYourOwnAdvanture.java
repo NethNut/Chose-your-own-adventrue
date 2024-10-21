7bbf2846503fbd8193cc86da58aa4bdf2f95b33d
NethNut <noah.smith27@student.tamdistrict.org>
2024-10-21 12:04:56 -0700

    added final if statments

--- ChoseYourOwnAdvanture.java
+++ ChoseYourOwnAdvanture.java
@@ -15,9 +15,34 @@ public class ChoseYourOwnAdvanture {
             System.out.println("you take the yolk 1 or do you try to keep the yolk steady 2?");
             userInput = sc.nextLine();
             if (userInput.equals("2")) {
-                System.out.println("You spoted");
+                System.out.println("You spoted a runway do you either 1 fly over or 2 try to contact the radio");
+                userInput = sc.nextLine();
+                if (userInput.equals("2")) {
+                    System.out.println(
+                            "the radio answers do you try to get permishon to land 1 or try to land without permison 2");
+                    userInput = sc.nextLine();
+                    if (userInput.equals("1")) {
+                        System.out.println("you wait for permision to land the plane fling in circles");
+                        System.out.println(
+                                "you get the signal to land but they tell you thier is crosswind do you 1 land into the crosswing or 2 land aganst the crosswind");
+                        userInput = sc.nextLine();
+                        if (userInput.equals("1")) {
+                            System.err
+                                    .println("you slowly land and get to safty. congrats you survived the incident!!!");
+                        } else {
+                            System.out.println(
+                                    "you got pushed by the crosswind and over shot the runway killing yourslef you lose");
+                        }
+                    } else {
+                        System.out.println(
+                                "you try to land and end up creahsing into another plane while landing killing you");
+                    }
+                } else {
+                    System.out.println("you flew past it and ran out of fule crashing and dieing");
+                }
             } else {
-                System.out.println("the plane jerked and the wing fell off causing the plane to crash kill you");
+                System.out.println(
+                        "the plane jerked to the side and the wings fell of casuing you to crash killing you ");
             }
         } else {
             System.out.println("The pilot never woke up and you end up dying try again");
